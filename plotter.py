0d4d5cd3180a7df84e3ed8cf9a3faefce8811296
Make Magazin <cm@ix.de>
2020-02-05 10:16:10 +0100

    plotter.py und Tesla-Beispielbild hinzugefügt
    wie im Artikel beschrieben

---
+++ plotter.py
@@ -0,0 +1,13 @@
+from brachiograph import BrachioGraph
+
+bg = BrachioGraph(
+    inner_arm=8,           # Oberarmlänge cm        
+    outer_arm=8,           # Unterarmlänge cm
+    bounds=(-8, 4, 4, 13), # Koordinaten Zeichenbereich
+    servo_1_degree_ms=-10, # Bewegung Schulterservo
+    servo_2_degree_ms=10,  # Bewegung Ellenbogenservo
+    servo_1_centre=1500    # Mittelstellung Schulter
+    servo_2_centre=1500    # Mittelstellung Ellenbogen
+    pw_down=1850,          # Position Stift unten
+    pw_up=1500,            # Position Stift angehoben 
+)
